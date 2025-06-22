7201c0ffd057858dca3bfd1e8acff1fc941136b9
ChrisTopher <>
2025-06-22 18:43:47 +0200

    finally a working example

--- backend/src/main/java/com/sudoku/backend/controllers/SudokuController.java
+++ backend/src/main/java/com/sudoku/backend/controllers/SudokuController.java
@@ -5,13 +5,20 @@ import com.sudoku.backend.jpa.entities.Sudoku;
 import com.sudoku.backend.jpa.repository.ContinueLaterRepository;
 import com.sudoku.backend.jpa.repository.SudokuRepository;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.swing.text.html.Option;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.logging.Logger;
 
 @RestController
+//@RequestMapping("sudoku")
 public class SudokuController {
     private final SudokuRepository sudokuRepository;
     private final ContinueLaterRepository continueLaterRepository;
@@ -23,40 +30,94 @@ public class SudokuController {
 
     private static final Logger LOGGER = Logger.getLogger(SudokuController.class.getName());
 
-    @GetMapping("/sudoku/{id}")
-    public Optional<Sudoku> getSudoku(@PathVariable int id) {
-        Optional<Sudoku> sudoku = sudokuRepository.findById(id);
-        if(sudoku.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No Sudoku found for user with id: " + id);
-        }
-        return sudoku;
+    @GetMapping("{difficulty}")
+    public ResponseEntity<Sudoku> getSudokuById(@PathVariable int difficulty) {
+        // TODO: generate sudokus by difficulty
+        return ResponseEntity.of(Optional.of(new Sudoku()));
     }
 
-    @PostMapping("sudoku/save/{userId}")
-    public Sudoku saveForLater(@RequestBody Sudoku sudoku, @PathVariable String userId) {
+    @GetMapping("/continue/{userId}")
+    public ResponseEntity<Sudoku> getSudokuToContinue(@PathVariable String userId) {
+        // TODO: needs to be adapted for multiple sudokus for later
+        Optional<ContinueLater> continueLater = continueLaterRepository.findByUserId(userId);
+        if (continueLater.isPresent() && continueLater.get().getSudokuId() != null) {
+            Optional<Sudoku> sudoku = sudokuRepository.findById(continueLater.get().getSudokuId());
+            if (sudoku.isPresent()) {
+                return ResponseEntity.of(sudoku);
+            } else {
+                continueLaterRepository.deleteById(continueLater.get().getId());
+            }
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("save/{userId}") // TODO: get userId from token later on
+    public ResponseEntity<Sudoku> saveForUser(@RequestBody Sudoku sudoku, @PathVariable String userId) {
+        // TODO: needs to be adapted for multiple sudokus for later
         Optional<ContinueLater> optionalContinueLater = continueLaterRepository.findByUserId(userId);
-        if(optionalContinueLater.isEmpty()) {
+        if (optionalContinueLater.isEmpty()) {
+            // TODO: maybe make it transactional
             Sudoku newSudoku = sudokuRepository.save(sudoku);
             continueLaterRepository.save(new ContinueLater(userId, newSudoku.getId()));
-            return sudokuRepository.save(sudoku);
-        }
-        else {
-            ContinueLater jtUserSudoku = optionalContinueLater.get();
-            if(jtUserSudoku.getSudokuId() != null) {
-                sudokuRepository.deleteById(jtUserSudoku.getSudokuId());
+            return ResponseEntity.of(Optional.of(newSudoku));
+        } else {
+            // TODO: maybe make it transactional
+            ContinueLater continueLater = optionalContinueLater.get();
+            if (continueLater.getSudokuId() != null) {
+                sudokuRepository.deleteById(continueLater.getSudokuId());
             }
             Sudoku newSudoku = sudokuRepository.save(sudoku);
-            jtUserSudoku.setSudokuId(newSudoku.getId());
-            continueLaterRepository.save(jtUserSudoku);
-            return newSudoku;
+            continueLater.setSudokuId(newSudoku.getId());
+            continueLaterRepository.save(continueLater);
+            return ResponseEntity.of(Optional.of(newSudoku));
         }
     }
 
-    // TODO find out how to use keycloak token to identify user
+    @DeleteMapping("cancel/{userId}") // TODO: get userId from token later on
+    public ResponseEntity<Sudoku> cancelSudokuToContinue(@PathVariable String userId) {
+        Optional<ContinueLater> continueLater = continueLaterRepository.findByUserId(userId);
+        if (continueLater.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        Optional<Sudoku> sudoku = sudokuRepository.findById(continueLater.get().getSudokuId());
+        if (sudoku.isPresent()) {
+            sudokuRepository.deleteById(sudoku.get().getId());
+            continueLaterRepository.deleteById(continueLater.get().getId());
+            return ResponseEntity.of(sudoku);
+        }
+        continueLaterRepository.deleteById(continueLater.get().getId());
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("submissions")
+    public ResponseEntity<List<Sudoku>> getAllSubmissions() {
+        List<Sudoku> submissions = sudokuRepository.findAllWhereSubmitterIdIsNotNull();
+        return ResponseEntity.ok(submissions);
+    }
+
+    // TODO: find out how to use keycloak token to identify user
     // even though i can just add it in path bla bla bla
-    @PostMapping("/sudoku/submission{userId}")
-    public Sudoku saveSudoku(@RequestBody Sudoku sudoku, @PathVariable String userId) {
+    @PostMapping("submissions/{userId}")
+    public ResponseEntity<Sudoku> saveSubmittedSudoku(@RequestBody Sudoku sudoku, @PathVariable String userId) {
         sudoku.setSubmitterId(userId);
-        return sudokuRepository.save(sudoku);
+        Sudoku submittedSudoku = sudokuRepository.save(sudoku);
+        return ResponseEntity.of(Optional.of(submittedSudoku));
+    }
+
+    // TODO: find out how to use keycloak token to identify user
+    // even though i can just add it in path bla bla bla
+    @DeleteMapping("submissions/{submissionId}/{userId}")
+    public ResponseEntity<Sudoku> deleteSubmittedSudoku(@PathVariable Integer submissionId, @PathVariable String userId) {
+        Optional<Sudoku> submission = sudokuRepository.findByIdAndSubmitterId(submissionId, userId);
+        if (submission.isPresent()) {
+            sudokuRepository.deleteById(submissionId);
+            return ResponseEntity.of(submission);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/protected/premium")
+    public ResponseEntity<String> test(@AuthenticationPrincipal Jwt jwt) {
+        return ResponseEntity.of(Optional.of("test"));
     }
 }
--- backend/src/main/java/com/sudoku/backend/jpa/repository/SudokuRepository.java
+++ backend/src/main/java/com/sudoku/backend/jpa/repository/SudokuRepository.java
@@ -1,7 +1,15 @@
 package com.sudoku.backend.jpa.repository;
 
 import com.sudoku.backend.jpa.entities.Sudoku;
+import org.springframework.data.jpa.repository.NativeQuery;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface SudokuRepository extends CrudRepository<Sudoku, Integer> {
+    @NativeQuery("SELECT * FROM sudoku WHERE submitterId IS NOT NULL")
+    public List<Sudoku> findAllWhereSubmitterIdIsNotNull();
+    public Optional<Sudoku> findByIdAndSubmitterId(Integer id, String submitterId);
 }
